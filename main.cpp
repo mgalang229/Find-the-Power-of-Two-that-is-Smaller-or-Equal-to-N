82472490e07f00d725ab51e5bd377619b75fff4d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-27 11:51:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main() {
     cin >> N;
     int exponent = 0;
     for (int i = 0; i < 30; i++) {
+        // divide 'N' by increasing powers of 2 to find the
+        // the maximum number (that is a power of 2) that can
+        // still divide 'N'
         if (((N >> i) & 1) == 1) {
             exponent = i;
         }
